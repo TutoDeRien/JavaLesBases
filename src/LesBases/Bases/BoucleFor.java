1d8c6a641c4a862eab2ecdcf51c4eef45f777509
TutoDeRien <daniel@tutoderien.com>
2019-08-26 20:25:52 +0200

    change in the for loop the number of 2 to 5

--- src/LesBases/Bases/BoucleFor.java
+++ src/LesBases/Bases/BoucleFor.java
@@ -7,7 +7,7 @@ public class BoucleFor {
         for (int nombre = 2;nombre<100;nombre++){
             boolean test = true;
 
-            for (int start = 2;start<nombre;start++){
+            for (int start = 5;start<nombre;start++){
                 if (nombre%start==0){
                     test = false;
                     break;
